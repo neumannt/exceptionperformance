d8ccff6137e69f1adb73182d4381e1a53d59f373
Thomas Neumann <thomas.neumann@in.tum.de>
2022-02-09 17:18:17 +0100

    use more expressive error codes by default

--- outcome.cpp
+++ outcome.cpp
@@ -4,16 +4,24 @@
 
 namespace outcome = outcome_v2_e261cebd;
 
+#if 0
+// This here is faster, but really minimal in expressiveness
 enum class ErrorCode {
     InvalidValue
 };
-
 template<typename T>
 using result = outcome::result<T, ErrorCode, outcome::policy::all_narrow>;
+#define DOTHROW() return ErrorCode::InvalidValue
+#else
+// This is better comparable to the other approaches
+template<typename T>
+using result = outcome::result<T>;
+#define DOTHROW() return std::make_error_code(std::errc::argument_out_of_domain)
+#endif
 
 static result<void> doSqrt(std::span<double> values) noexcept {
     for (auto &v: values) {
-        if (v < 0) return ErrorCode::InvalidValue;
+        if (v < 0) DOTHROW();
         v = sqrt(v);
     }
     return outcome::success();
@@ -30,7 +38,7 @@ unsigned outcomeResultSqrt(std::span<double> values, unsigned repeat) noexcept {
 
 static result<unsigned> doFib(unsigned n, unsigned maxDepth) noexcept __attribute((noinline, optimize("no-optimize-sibling-calls")));
 static result<unsigned> doFib(unsigned n, unsigned maxDepth) noexcept {
-    if (!maxDepth) return ErrorCode::InvalidValue;
+    if (!maxDepth) DOTHROW();
     if (n <= 2) return 1;
     auto n2 = OUTCOME_TRYX(doFib(n - 2, maxDepth - 1));
     auto n1 = OUTCOME_TRYX(doFib(n - 1, maxDepth - 1));
